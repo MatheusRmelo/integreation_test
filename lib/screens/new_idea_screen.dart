a3bd9c3b11a4d057acc118e9695ee1a825e341c2
Matheus Melo <matheusroberttjmelo@gmail.com>
2022-06-13 17:20:39 -0300

    Use tutorial https://www.raywenderlich.com/29321816-integration-testing-in-flutter-getting-started like source

--- lib/screens/new_idea_screen.dart
+++ lib/screens/new_idea_screen.dart
@@ -42,8 +42,7 @@ class NewIdeaScreen extends StatefulWidget {
 
 class _NewIdeaScreenState extends State<NewIdeaScreen> {
   final TextEditingController _ideaNameController = TextEditingController();
-  final TextEditingController _ideaInspirationController =
-      TextEditingController();
+  final TextEditingController _ideaInspirationController = TextEditingController();
   final _newIdeaFormKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
@@ -139,12 +138,10 @@ class _NewIdeaScreenState extends State<NewIdeaScreen> {
                       ),
                       onPressed: () {
                         if (_newIdeaFormKey.currentState!.validate()) {
-                          Provider.of<IdeasModel>(context, listen: false)
-                              .addIdea(
+                          Provider.of<IdeasModel>(context, listen: false).addIdea(
                             Idea(
                               title: _ideaNameController.value.text.trim(),
-                              inspiration:
-                                  _ideaInspirationController.value.text.trim(),
+                              inspiration: _ideaInspirationController.value.text.trim(),
                               date: DateTime.now().toIso8601String(),
                             ),
                           );
